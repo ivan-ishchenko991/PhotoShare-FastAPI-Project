6612afefd44d1eef4314ff2dd9303143de45ed06
ivan-ishchenko991 <lvan.lshchenko19.01@gmail.com>
2023-11-05 17:06:43 +0200

    Update comments.py

--- src/routes/comments.py
+++ src/routes/comments.py
@@ -2,26 +2,31 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from datetime import datetime
 from src.database.connect import get_db
-from src.repository.comments import create_comment, get_comments_by_photo_id, get_comment_by_id, update_comment, delete_comment
+from src.database.models import User
+from src.repository.comments import create_comment, get_comments_by_photo_id, get_comment_by_id, update_comment, \
+    delete_comment
 from src.schemas import CommentBase, CommentModel
 from src.services.auth import auth_service
 
 router = APIRouter(tags=["comments"])
 
+
 @router.post("/{photos_id}", response_model=CommentModel)
 def create_comment(
-    photos_id: int,
-    body: CommentBase,
-    db: Session = Depends(get_db),
-    current_user: User = Depends(auth_service.get_current_user)
+        photos_id: int,
+        body: CommentBase,
+        db: Session = Depends(get_db),
+        current_user: User = Depends(auth_service.get_current_user)
 ):
     new_comment = create_comment(photos_id, body, db, current_user)
     return new_comment
+
 
 @router.get("/{photo_id}/", response_model=list[CommentModel])
 def get_comments(photo_id: int, db: Session = Depends(get_db)):
     comments = get_comments_by_photo_id(db, photo_id)
     return comments
+
 
 @router.get("/{comment_id}/", response_model=CommentModel)
 def get_comment(comment_id: int, db: Session = Depends(get_db)):
@@ -30,12 +35,13 @@ def get_comment(comment_id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Comment not found")
     return comment
 
+
 @router.put("/{comment_id}", response_model=CommentModel)
 def update_comment(
-    comment_id: int,
-    comment: CommentBase,
-    user: User = Depends(auth_service.get_current_user),
-    db: Session = Depends(get_db)
+        comment_id: int,
+        comment: CommentBase,
+        user: User = Depends(auth_service.get_current_user),
+        db: Session = Depends(get_db)
 ):
     db_comment = get_comment_by_id(db, comment_id)
     if not db_comment:
@@ -47,11 +53,12 @@ def update_comment(
     updated_comment = update_comment(db, comment_id, comment_data)
     return updated_comment
 
+
 @router.delete("/{comment_id}/", response_model=CommentModel)
 def remove_comment(
-    comment_id: int,
-    db: Session = Depends(get_db),
-    current_user: User = Depends(auth_service.get_current_user)
+        comment_id: int,
+        db: Session = Depends(get_db),
+        current_user: User = Depends(auth_service.get_current_user)
 ):
     if not current_user:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Not authorized")
@@ -61,8 +68,8 @@ def remove_comment(
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Comment not found")
 
     if (
-        auth_service.is_admin(current_user) or
-        auth_service.is_moderator(current_user)
+            auth_service.is_admin(current_user) or
+            auth_service.is_moderator(current_user)
     ):
         deleted_comment = delete_comment(db, comment_id)
         return deleted_comment
